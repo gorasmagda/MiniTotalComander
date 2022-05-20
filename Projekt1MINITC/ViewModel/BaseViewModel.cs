9ff19c09f9e4d31af2ae3d30587889a67f1a2d90
Magdalena Góras <magdgor933@student.polsl.pl>
2022-05-20 13:46:25 +0200

    Without Copy button

---
+++ Projekt1MINITC/ViewModel/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1MINITC.ViewModel
+{
+    class BaseViewModel : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged(params string[] propertiesNames)
+            {
+                if (PropertyChanged != null)
+                {
+                    foreach (var prop in propertiesNames)
+                    {
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+                    }
+                }
+            }
+
+        }
+    }
+
+
--- Projekt1MINITC/ViewModel/PanelViewModel.cs
+++ Projekt1MINITC/ViewModel/PanelViewModel.cs
@@ -10,9 +10,9 @@ using System.Windows.Input;
 
 namespace Projekt1MINITC.ViewModel
 {
-    class PanelViewModel : INotifyPropertyChanged
+    class PanelViewModel : BaseViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        
         public ObservableCollection<string> Drives { get; set; }
         public ObservableCollection<string> Folders { get; set; }
 
@@ -29,13 +29,97 @@ namespace Projekt1MINITC.ViewModel
             {
                 Drives.Clear();
                 string[] drives = Directory.GetLogicalDrives();
-
+               
                 foreach (var x in drives)
-                    Drives.Add(x);
+                  Drives.Add(x);
                 
             }
             , null));
 
+        private string selectedDrive;
+        public string SelectedDrive
+        {
+            get => selectedDrive;
+            set
+            {
+                selectedDrive = value;
+                Sciezka = SelectedDrive;
+                OnPropertyChanged(nameof(SelectedDrive));
+               
+            }
+        }
+
+        private string sciezka;
+
+        public string Sciezka
+        {
+            get => sciezka;
+            set
+            {
+                sciezka = value;
+                Folders.Clear();
+                if(Sciezka != null)
+                {
+                    try
+                    {
+                        string[] folderlist = Directory.GetDirectories(Sciezka);
+                        string[] filelist = Directory.GetFiles(Sciezka);
+                        if(Sciezka.Length > 3)
+                        {
+                            Folders.Add("..");
+                        }
+                        foreach(string f in folderlist)
+                        {
+                            Folders.Add("<D>" + Path.GetFileName(f));
+                        }
+                        foreach(string file in filelist)
+                        {
+                            Folders.Add(Path.GetFileName(file));
+                        }
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("ERROR!");
+                        Sciezka = Path.GetDirectoryName(Path.GetDirectoryName(Sciezka));
+                    }
+                    OnPropertyChanged(nameof(Sciezka));
+                }
+
+            }
+        }
+
+        private string selectedFolder;
+
+        public string SelectedFolder
+        {
+            get => selectedFolder;
+            set
+            {
+                selectedFolder = value;
+                OnPropertyChanged(nameof(selectedFolder));
+            }
+        }
+
+        //ListBoxDoubleClick
+        private ICommand listBoxDClick;
+        public ICommand ListBoxDClick => listBoxDClick ?? (listBoxDClick = new RelayCommand(
+            o => {
+                if (SelectedFolder.Equals(".."))
+                {
+                    Sciezka = Path.GetDirectoryName(Path.GetDirectoryName(Sciezka));
+                    if (!Sciezka.EndsWith("\\"))
+                    {
+                        Sciezka += "\\";
+                    }
+                }
+                else if (SelectedFolder.StartsWith("<D>"))
+                {
+                    Sciezka += SelectedFolder.Remove(0, 3) + "\\";
+                }
+                SelectedFolder = null;
+            }
+            , null));
 
 
     }
